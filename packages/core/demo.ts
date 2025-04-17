7c0e20beb980b2493e00b1fd58cd8406d5641f26
Vladimir Jimenez <allejo@me.com>
2025-04-17 00:32:31 -0700

    feat: import Joann API fabric + variant data

--- packages/core/demo.ts
+++ packages/core/demo.ts
@@ -1,7 +1,7 @@
-import { PrismaClient } from '@xocaid/sewwise-dto/src/generated'
+import {PrismaClient} from '@xocaid/sewwise-dto/src/generated'
 
-import { parse } from 'jsonlines';
-import { createReadStream } from 'node:fs';
+import {parse} from 'jsonlines';
+import {createReadStream} from 'node:fs';
 
 interface UglyData {
 	Available: boolean
@@ -63,8 +63,70 @@ interface UglyData {
 	variation_id: string
 }
 
+export interface JSONLData {
+	data: UglyData
+	is_slotted: boolean
+	labels: {}
+	matched_terms: any[]
+	value: string
+	variations: { data: UglyData, value: string }[]
+}
+
 const parser = parse();
 const prisma = new PrismaClient();
+
+function createOrFind(uglyData: UglyData) {
+	return prisma.fabric.upsert({
+		where: {
+			externalId: uglyData.id,
+		},
+		update: {},
+		create: {
+			externalId: uglyData.id,
+			itemNumber: uglyData.ID + '',
+			url: uglyData.master_product_url,
+			name: uglyData.master_product_name,
+			description: uglyData.description,
+			basePrice: uglyData["Base Price"] ?? -1,
+			rating: uglyData.bvAverageRating ?? uglyData.master_rating ?? -1,
+			ratingCount: uglyData.bvReviewCount ?? -1,
+			ratingMax: 5,
+			tags: uglyData.group_ids,
+			features: uglyData.productFeatures,
+			seller: {
+				connectOrCreate: {
+					where: {
+						name: 'Joann'
+					},
+					create: {
+						name: 'Joann'
+					}
+				}
+			},
+			category: {
+				connectOrCreate: {
+					where: {
+						externalId: uglyData.categoryNumber + ''
+					},
+					create: {
+						externalId: uglyData.categoryNumber + '',
+						description: uglyData.categoryDescription,
+					}
+				}
+			},
+			brand: {
+				connectOrCreate: {
+					where: {
+						name: uglyData.brand || 'Unknown',
+					},
+					create: {
+						name: uglyData.brand || 'Unknown',
+					}
+				}
+			}
+		},
+	})
+}
 
 (async function () {
 	// const patternSearch = new PatternSearchQuery();
@@ -72,59 +134,108 @@ const prisma = new PrismaClient();
 	//
 	// console.log(JSON.stringify(patternResponse.products));
 
-	parser.on('data', ({ data: uglyData }: { data: UglyData }) => {
-		const fabric = prisma.fabric.create({
-			data: {
-				externalId: uglyData.id,
-				itemNumber: uglyData.ID + '',
-				url: uglyData.master_product_url,
-				name: uglyData.master_product_name,
-				description: uglyData.description,
-				basePrice: uglyData["Base Price"],
-				rating: uglyData.bvAverageRating ?? uglyData.master_rating ?? -1,
-				ratingCount: uglyData.bvReviewCount ?? -1,
-				ratingMax: 5,
-				tags: uglyData.group_ids,
-				features: uglyData.productFeatures,
-				seller: {
-					connectOrCreate: {
-						where: {
-							name: 'Joann'
-						},
-						create: {
-							name: 'Joann'
-						}
-					}
-				},
-				category: {
-					connectOrCreate: {
-						where: {
-							externalId: uglyData.categoryNumber + ''
-						},
-						create: {
-							externalId: uglyData.categoryNumber + '',
-							description: uglyData.categoryDescription,
-						}
-					}
-				},
-				brand: {
-					connectOrCreate: {
-						where: {
-							name: uglyData.brand || 'Unknown',
-						},
-						create: {
-							name: uglyData.brand || 'Unknown',
-						}
-					}
-				}
-			},
-		}).then(result => {
-			console.log('Created fabric:', result);
-		})
+	let counter = 0;
 
-		// console.log(JSON.stringify(uglyData, null, 2));
+	parser.on('data', ({data: uglyData, variations}: JSONLData) => {
+		console.log('> Processing', counter++, uglyData.id, uglyData.name, uglyData.variationType);
+
+		createOrFind(uglyData)
+			.then(fabric => {
+				if (variations == null) {
+					return Promise.resolve();
+				}
+
+				let hasMessageDisplayed = false;
+				const variantPromises: Promise<unknown>[] = [];
+
+				for (let {data: variant} of variations) {
+					if (variant.variationType === 'simple') {
+						break;
+					}
+
+					if (!hasMessageDisplayed) {
+						console.log('>> Found variant', variant.refinementColors);
+						hasMessageDisplayed = true;
+					}
+
+					const variantModelPromise = prisma.fabricVariant.upsert({
+						where: {
+							externalId: variant.ID + '',
+						},
+						update: {
+							colors: variant.refinementColors?.split('|'),
+						},
+						create: {
+							externalId: variant.ID + '',
+							name: variant.name,
+							url: variant.url,
+							swatchImage: {
+								connectOrCreate: {
+									where: {
+										originalUrl: variant.swatch_image_url,
+									},
+									create: {
+										originalUrl: variant.swatch_image_url,
+										relativeUrl: '',
+									}
+								}
+							},
+							image: {
+								connectOrCreate: {
+									where: {
+										originalUrl: variant.image_url,
+									},
+									create: {
+										originalUrl: variant.image_url,
+										relativeUrl: '',
+									}
+								}
+							},
+							fabric: {
+								connect: {
+									id: fabric.id
+								}
+							},
+						}
+					});
+
+					variantPromises.push(variantModelPromise);
+				}
+
+				return Promise.all(variantPromises);
+			})
 	});
 
-	const readStream = createReadStream('fabrics.jsonl');
+	const files = [
+		"fabrics.jsonl",
+		"fabrics-Anti-Pill Plush Fleece.jsonl",
+		"fabrics-Blizzard Fleece.jsonl",
+		"fabrics-calico-cotton.jsonl",
+		"fabrics-Fabric Quarter Singles.jsonl",
+		"fabrics-flannel.jsonl",
+		"fabrics-Keepsake Calico Cotton Fabric.jsonl",
+		"fabrics-Kids Licensed Fabric.jsonl",
+		"fabrics-Knit Fabric.jsonl",
+		"fabrics-License-Cotton.jsonl",
+		"fabrics-Novelty Cotton Fabric.jsonl",
+		"fabrics-Premium Cotton Fabric.jsonl",
+		"fabrics-Quilt Cotton Fabric.jsonl",
+		"fabrics-Quilter's Showcase Cotton Fabric.jsonl",
+		"fabrics-Super Snuggle Flannel.jsonl",
+		"fabrics-Team Cotton.jsonl",
+	];
+
+	// files.forEach(file => {
+	// 	console.log('>>', file);
+	//
+	// 	const readStream = createReadStream(file);
+	// 	readStream.pipe(parser);
+	// })
+
+	const readStream = createReadStream(files[15]);
 	readStream.pipe(parser);
-})();
+
+	// parser.end();
+})().then(() => {
+	console.log('Finished.')
+});
